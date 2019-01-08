b1306395eabeea6ea5c91a179f3edfd4274fc999
Matěj Kneifl <matej.kneifl@seznam.cz>
2019-01-08 12:04:02 +0100

    Create index.php
    FNGUJE:-----------)

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="cs" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ay:-)</title>
+  </head>
+  <body>
+     <?php
+        $number = 0;
+        while ($number <> 10) {
+          $number=rand(0,20);
+          echo $number . "<br>";
+        }
+      ?>
+  </body>
+  </html>
